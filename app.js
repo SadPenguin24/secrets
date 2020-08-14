70d2fb377af7b6b48f0426c958fe635a1f3deced
Dan <danhendri.pro@gmail.com>
2020-08-14 16:50:41 +0800

    Modify login.ejs

--- app.js
+++ app.js
@@ -195,6 +195,9 @@ app.post("/login", function(req, res) {
 });
 
 let port = process.env.PORT;
+if (port == null || port == "") {
+    port = 3000;
+};
 
 app.listen(port, function() {
     console.log("Server Started Successfully");
